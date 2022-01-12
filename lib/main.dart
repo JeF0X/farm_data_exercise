9ecf9c8aedd66f61e1080adf39e1e1cf03e63461
Juho Kettunen <jekettu@gmail.com>
2022-01-12 17:54:50 +0200

    Line chart base

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
+import 'package:farm_data_exercise/widgets/line_chart.dart';
 import 'package:farm_data_exercise/widgets/measurement_gauge.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final usData = [
+  TimeSeriesValues(DateTime(2017, 9, 19), 5),
+  TimeSeriesValues(DateTime(2017, 9, 26), 25),
+  TimeSeriesValues(DateTime(2017, 10, 3), 78),
+  TimeSeriesValues(DateTime(2017, 10, 10), 54),
+];
+
+final ukData = [
+  TimeSeriesValues(DateTime(2017, 9, 19), 15),
+  TimeSeriesValues(DateTime(2017, 9, 26), 33),
+  TimeSeriesValues(DateTime(2017, 10, 3), 68),
+  TimeSeriesValues(DateTime(2017, 10, 10), 48),
+];
+
+final fiData = [
+  TimeSeriesValues(DateTime(2017, 9, 19), 12.3),
+  TimeSeriesValues(DateTime(2017, 9, 26), 33.5),
+  TimeSeriesValues(DateTime(2017, 10, 3), 70),
+  TimeSeriesValues(DateTime(2017, 10, 10), 24.0),
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -36,18 +58,29 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: SizedBox(
-            width: 200,
-            height: 200,
-            child: MeasurementGauge(
-              value: 6.4,
-              text: 'Ph',
-              minValue: 0,
-              maxValue: 14,
-              secondSegmentStartValue: 4.5,
-              thirdSegmentStartValue: 7.0,
-            )),
+      body: Center(
+        child: Column(
+          children: [
+            Card(
+              child: const SizedBox(
+                width: 200,
+                height: 200,
+                child: MeasurementGauge(
+                  value: 14,
+                  text: 'Ph',
+                  minValue: 0,
+                  maxValue: 14,
+                  secondSegmentStartValue: 4.5,
+                  thirdSegmentStartValue: 7.0,
+                ),
+              ),
+            ),
+            LineChart(
+              data: [usData, ukData, fiData],
+              textStart: 'ph',
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/line_chart.dart
@@ -0,0 +1,128 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+
+class LineChart extends StatefulWidget {
+  final List<List<TimeSeriesValues>> data;
+  final bool animate;
+  final String textStart;
+  final String textBottom;
+  final String textEnd;
+  final double maxHeight;
+
+  const LineChart({
+    Key? key,
+    this.animate = false,
+    required this.data,
+    this.textStart = '',
+    this.textBottom = '',
+    this.textEnd = '',
+    this.maxHeight = 300.0,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _LineChartState();
+}
+
+class _LineChartState extends State<LineChart> {
+  DateTime? _time;
+  Map<String, num> _measures = {};
+  List<charts.Series<TimeSeriesValues, DateTime>> seriesList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    seriesList = _createSeriesList(widget.data);
+  }
+
+  static List<charts.Series<TimeSeriesValues, DateTime>> _createSeriesList(
+      List<List<TimeSeriesValues>> data) {
+    List<charts.Series<TimeSeriesValues, DateTime>> seriesList = [];
+    int index = 0;
+    for (var item in data) {
+      seriesList.add(
+        charts.Series<TimeSeriesValues, DateTime>(
+          id: index.toString(),
+          domainFn: (TimeSeriesValues sales, _) => sales.time,
+          measureFn: (TimeSeriesValues sales, _) => sales.sales,
+          data: item,
+        ),
+      );
+      index++;
+    }
+    return seriesList;
+  }
+
+  _onSelectionChanged(charts.SelectionModel model) {
+    final selectedDatum = model.selectedDatum;
+
+    DateTime? time;
+    final measures = <String, num>{};
+
+    if (selectedDatum.isNotEmpty) {
+      time = selectedDatum.first.datum.time;
+      for (var datumPair in selectedDatum) {
+        measures[datumPair.series.displayName!] = datumPair.datum.sales;
+      }
+    }
+
+    setState(() {
+      _time = time;
+      _measures = measures;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> children = [
+      Expanded(
+        child: charts.TimeSeriesChart(
+          seriesList,
+          animate: widget.animate,
+          behaviors: [
+            charts.ChartTitle(widget.textStart,
+                behaviorPosition: charts.BehaviorPosition.start,
+                titleOutsideJustification:
+                    charts.OutsideJustification.middleDrawArea),
+            charts.ChartTitle(widget.textBottom,
+                behaviorPosition: charts.BehaviorPosition.bottom,
+                titleOutsideJustification:
+                    charts.OutsideJustification.middleDrawArea),
+            charts.ChartTitle(widget.textEnd,
+                behaviorPosition: charts.BehaviorPosition.end,
+                titleOutsideJustification:
+                    charts.OutsideJustification.middleDrawArea),
+          ],
+          selectionModels: [
+            charts.SelectionModelConfig(
+              type: charts.SelectionModelType.info,
+              changedListener: _onSelectionChanged,
+            )
+          ],
+        ),
+      ),
+    ];
+
+    if (_time != null) {
+      children.add(Padding(
+          padding: const EdgeInsets.only(top: 5.0),
+          child: Text(_time.toString())));
+    }
+    _measures.forEach((String series, num value) {
+      children.add(Text('$series: $value'));
+    });
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxHeight: widget.maxHeight),
+      child: Column(
+        children: children,
+      ),
+    );
+  }
+}
+
+class TimeSeriesValues {
+  final DateTime time;
+  final double sales;
+
+  TimeSeriesValues(this.time, this.sales);
+}
