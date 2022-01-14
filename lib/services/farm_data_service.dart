dfc284f7e6295767c9a78c5b84711f94207b8c23
Juho Kettunen <jekettu@gmail.com>
2022-01-14 15:32:06 +0200

    Functions for API points

--- lib/services/farm_data_service.dart
+++ lib/services/farm_data_service.dart
@@ -1,7 +1,9 @@
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:farm_data_exercise/models/aggregated_sensor_data.dart';
 import 'package:farm_data_exercise/models/farm.dart';
+import 'package:farm_data_exercise/models/sensor_data.dart';
 import 'package:farm_data_exercise/services/http_service.dart';
 
 class FarmDataService {
@@ -25,6 +27,62 @@ class FarmDataService {
         log(data.runtimeType.toString());
         throw Exception('Json data was not properly formatted');
       }
+    } on HttpException catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<Farm> getFarmInfo(int farmId) async {
+    try {
+      var url = _apiUrl + 'farms/$farmId';
+      var data = await httpService.sendRequest(url);
+      if (data is Map<String, dynamic>) {
+        Farm farm = Farm.fromJson(data);
+        return farm;
+      } else {
+        log(data.runtimeType.toString());
+        throw Exception('Json data was not properly formatted');
+      }
+    } on HttpException catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<List<SensorData>> getFarmStats(int farmId) async {
+    try {
+      var url = _apiUrl + 'farms/$farmId/stats';
+      var data = await httpService.sendRequest(url);
+      if (data is! Map<String, dynamic>) {
+        throw Exception('Json data was not properly formatted');
+      }
+      if (data['measurements'] is! List<dynamic>) {
+        throw Exception('Json data was not properly formatted');
+      }
+
+      List<SensorData> stats = [];
+      for (var item in data['measurements']) {
+        stats.add(SensorData.fromJson(item));
+      }
+      return stats;
+    } on HttpException catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<AggregatedSensorData> getFarmStatsMonthly(
+      int farmId, String sensorType) async {
+    try {
+      var url = _apiUrl + 'farms/$farmId/stats/$sensorType/monthly';
+      var data = await httpService.sendRequest(url);
+      if (data is! Map<String, dynamic>) {
+        throw Exception('Json data was not properly formatted');
+      }
+      var stats = AggregatedSensorData.fromJson(data);
+
+      return stats;
     } on HttpException catch (e) {
       log(e.toString());
       rethrow;
