20490dfb280fc74fcf6a9bc07c49b4d245d1c781
Juho Kettunen <jekettu@gmail.com>
2022-01-15 15:30:44 +0200

    Initial FarmsScreen

---
+++ lib/farms_screen.dart
@@ -0,0 +1,55 @@
+import 'package:farm_data_exercise/graphs_screen.dart';
+import 'package:farm_data_exercise/models/farm.dart';
+import 'package:farm_data_exercise/services/farm_data_service.dart';
+import 'package:farm_data_exercise/widgets/farm_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class FarmsScreen extends StatelessWidget {
+  const FarmsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    FarmDataService dataService = FarmDataService();
+    return Scaffold(
+      body: FutureBuilder<List<Farm>>(
+        future: dataService.getFarms(),
+        builder: (BuildContext context, AsyncSnapshot<List<Farm>> snapshot) {
+          List<Widget> children = [];
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: SizedBox(
+                height: 50.0,
+                width: 50.0,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          if (snapshot.hasData) {
+            List<Farm> farms = snapshot.data!;
+            for (var farm in farms) {
+              children.add(Card(
+                child: FarmListTile(
+                  farm: farm,
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => GraphsScreen(
+                        farm: farm,
+                      ),
+                    ),
+                  ),
+                ),
+              ));
+            }
+          }
+          return Center(
+            child: ListView(
+              children: children,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
